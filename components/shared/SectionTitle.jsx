88ccfb61e70ee569b73d08ef716bb996f48a33a5
md-yasin-miah <eiasin440@gmail.com>
2024-12-17 00:38:45 +0600

    feat: about us section

--- components/shared/SectionTitle.jsx
+++ components/shared/SectionTitle.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 
-const SectionTitle = ({ title,title2, subTitle, className=''}) => {
-  const tittleClass = "text-[46px] text-center relative font-rajdhani font-bold tracking-[5px] capitalize relative"  
+const SectionTitle = ({ title, title2, subTitle, placeholder_title, className = '', reverse = false }) => {
+  const tittleClass = "text-[46px] text-center relative font-rajdhani font-bold tracking-[5px] capitalize relative"
   return (
     <div className={`${className}`}>
       <h2 className={tittleClass}>
-         {title} <span className='bg-clip-text bg-gradient-to-r from-primary to-secondary text-transparent'>{title2}</span>
-         <span className='uppercase text-7xl text-primary opacity-10 tracking-[10px] font-bold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>about us</span>
-       </h2>
-    <p className="text-base text-center font-poppins leading-6 pl-8">{subTitle}</p>
+        {
+          reverse ?
+            <span>
+              <span className='bg-clip-text bg-gradient-to-r from-primary to-secondary text-transparent'>{title2}</span> {title}
+            </span>
+            :
+            <span>
+              {title} <span className='bg-clip-text bg-gradient-to-r from-primary to-secondary text-transparent'>{title2}</span>
+            </span>
+        }
+        <span className='uppercase whitespace-nowrap text-7xl text-primary opacity-10 tracking-[10px] font-bold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>{placeholder_title}</span>
+      </h2>
+      <p className="text-base text-center font-poppins leading-6 pl-8">{subTitle}</p>
     </div>
   )
 }
