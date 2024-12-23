b8b69ed62e9eb97e8bc6db43dfe87842a5210ebb
md-yasin-miah <eiasin440@gmail.com>
2024-12-23 16:28:29 +0600

    feat: our service page

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,8 +1,115 @@
+"use client"
+import SectionTitle from '@/components/shared/SectionTitle'
 import React from 'react'
+import { motion } from 'framer-motion'
+import Image from 'next/image'
+import { HoverEffect } from '@/components/ui/card-hover-effect'
+import { GridBackground } from '@/components/ui/grid-background'
+import image from '../../public/Home-BroadBand-Internet-scaled.webp'
+
+const services = [
+  {
+    title: "Home Broadband Internet",
+    description: "We are one of the most reliable broadband internet",
+    link: "/services/home-internet",
+    image: image
+  },
+  {
+    title: "Corporate Broadband Internet",
+    description: "Corporate and SME customers dedicated internet connections to ensure network stability.",
+    link: "/services/corporate-internet",
+    image: image
+  },
+  {
+    title: "Complete Network Solutions",
+    description: "Our network engineers work with our clients to provide the most efficient network solutions possible.",
+    link: "/services/network-solutions",
+    image: image
+  },
+  {
+    title: "Home Broadband Internet",
+    description: "We are one of the most reliable broadband internet and network for both gamers and regular users.",
+    link: "/services/home-internet",
+    image: image
+  },
+  {
+    title: "Corporate Broadband Internet",
+    description: "Corporate and SME customers dedicated internet connections to ensure network stability.",
+    link: "/services/corporate-internet",
+    image: image
+  },
+  {
+    title: "Complete Network Solutions",
+    description: "Our network engineers work with our clients to provide the most efficient network solutions possible.",
+    link: "/services/network-solutions",
+    image: image
+  }
+]
 
 const Services = () => {
   return (
-    <div>service</div>
+    <section className='relative py-20 overflow-hidden'>
+      <GridBackground animationDot={false}/>
+      <div className="relative z-10">
+        <SectionTitle
+          title="Our"
+          title2="services"
+          subTitle="Take a look at the services that we provide"
+        />
+
+        <div className="customContainer mt-10">
+          <HoverEffect effectBGClassName="bg-gradient-to-t from-primary to-secondary" items={services.map(service => ({
+            content: (
+              <div className="flex overflow-hidden flex-col items-center bg-gray-100 dark:bg-gray-400 rounded-[18px] h-full">
+                <motion.div
+                  className="relative w-full aspect-[4/3]"
+                  whileHover={{ scale: 1.05 }}
+                  transition={{ duration: 0.2 }}
+                >
+                  <Image
+                    src={service.image}
+                    alt={service.title}
+                    fill
+                    className="object-cover"
+                  />
+                </motion.div>
+                <div className='p-5 h-full flex flex-col justify-between'>
+                  <motion.div 
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                    transition={{ delay: 0.2 }}
+                  >
+                    <motion.h3 
+                      initial={{ opacity: 0, x: 20 }}
+                      whileInView={{ opacity: 1, x: 0 }}
+                      transition={{ duration: 0.5 }}
+                      className='text-lg font-semibold mb-2'
+                    >
+                      {service.title}
+                    </motion.h3>
+                    <motion.p
+                      initial={{ opacity: 0, x: 20 }}
+                      whileInView={{ opacity: 1, x: 0 }}
+                      transition={{ duration: 0.5 }}
+                      className='font-normal ellipsis-3'
+                    >
+                      {service.description}
+                    </motion.p>
+                  </motion.div>
+                  <motion.button
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.95 }}
+                    className="mt-4 px-6 py-2 bg-gradient-to-r from-primary to-secondary text-white rounded-lg w-fit"
+                  >
+                    Get a quote
+                  </motion.button>
+                </div>
+              </div>
+            )
+          }))} />
+        </div>
+      </div>
+    </section>
   )
 }
 
--- components/ui/card-hover-effect.jsx
+++ components/ui/card-hover-effect.jsx
@@ -5,7 +5,8 @@ import { useState } from "react";
 
 export const HoverEffect = ({
   items,
-  className
+  className,
+  effectBGClassName
 }) => {
   let [hoveredIndex, setHoveredIndex] = useState(null);
 
@@ -13,16 +14,16 @@ export const HoverEffect = ({
     (<div
       className={cn("grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3  py-10", className)}>
       {items.map((item, idx) => (
-        <Link
+        <div
           href={item?.link}
-          key={item?.link}
+          key={idx}
           className="relative group  block p-2 h-full w-full"
           onMouseEnter={() => setHoveredIndex(idx)}
           onMouseLeave={() => setHoveredIndex(null)}>
           <AnimatePresence>
             {hoveredIndex === idx && (
               <motion.span
-                className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] block  rounded-3xl"
+                className={`absolute -z-10 inset-0 h-full w-full ${effectBGClassName ? effectBGClassName:'bg-neutral-200 dark:bg-slate-800/[0.8]'} block  rounded-3xl`}
                 layoutId="hoverBackground"
                 initial={{ opacity: 0 }}
                 animate={{
@@ -35,50 +36,9 @@ export const HoverEffect = ({
                 }} />
             )}
           </AnimatePresence>
-          <Card>
-            <CardTitle>{item.title}</CardTitle>
-            <CardDescription>{item.description}</CardDescription>
-          </Card>
-        </Link>
+          {item.content}
+        </div>
       ))}
     </div>)
-  );
-};
-
-export const Card = ({
-  className,
-  children
-}) => {
-  return (
-    (<div
-      className={cn(
-        "rounded-2xl h-full w-full p-4 overflow-hidden bg-black border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
-        className
-      )}>
-      <div className="relative z-50">
-        <div className="p-4">{children}</div>
-      </div>
-    </div>)
-  );
-};
-export const CardTitle = ({
-  className,
-  children
-}) => {
-  return (
-    (<h4 className={cn("text-zinc-100 font-bold tracking-wide mt-4", className)}>
-      {children}
-    </h4>)
-  );
-};
-export const CardDescription = ({
-  className,
-  children
-}) => {
-  return (
-    (<p
-      className={cn("mt-8 text-zinc-400 tracking-wide leading-relaxed text-sm", className)}>
-      {children}
-    </p>)
   );
 };
