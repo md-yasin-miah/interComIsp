d6f1149e7ef8628802846ce4f1e99f90d64ee7ef
md-yasin-miah <eiasin440@gmail.com>
2025-01-13 18:34:18 +0600

    refactor: update environment variable usage for configuration consistency
    - Replaced NEXT_PUBLIC environment variables with standard environment variables in .env.local, next.config.mjs, and helper/config.js for improved clarity and consistency in configuration management.

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,8 @@ const nextConfig = {
     remotePatterns: [
       // "https://pb.netlandbd.com/api/files/pbc_181089699/2isu5pfo7878fzr/server_ikfp8eai43.lottie"
       {
-        protocol: process.env.NEXT_PUBLIC_PROTOCOL,
-        hostname: process.env.NEXT_PUBLIC_HOSTNAME,
+        protocol: process.env.PROTOCOL,
+        hostname: process.env.HOSTNAME,
         port: '',
         pathname: '/api/files/**',
       },
